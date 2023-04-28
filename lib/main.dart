948d2dcad88b98af7839e39d07ae239c6b2bd08c
kiplangatmike <m.kiplangat@alustudent.com>
2023-04-28 14:03:40 +0200

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_app/firebase_options.dart';
+import 'firebase_options.dart';
 import 'package:food_app/src/authentication/screens/register.dart';
 import 'package:food_app/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:get/get.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-      .then((value) => Get.put(AuthenticationRepository));
-  await Firebase.initializeApp();
+  // Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+  //     .then((value) => Get.put(AuthenticationRepository));
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Get.put(AuthenticationRepository());
   runApp(const MyApp());
 }
